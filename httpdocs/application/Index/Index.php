da37dfa368348217fd9635848a997caa5fbda7ad
Vic Cherubini <vmc@leftnode.com>
2010-01-31 10:53:00 -0600

    Got the track formatting length working better.

--- httpdocs/application/Index/Index.php
+++ httpdocs/application/Index/Index.php
@@ -7,6 +7,17 @@ class Index_Controller extends Root_Controller {
 	public function indexGet() {
 		$this->is_logged_in = ttu_user_is_logged_in();
 		
+		/* Get the latest and popular tracks. */
+		$this->latest_track_iterator = API::getDataModel()->where('status = ?', STATUS_ENABLED)
+			->orderBy('date_create', 'DESC')
+			->limit(10)
+			->loadAll(new Track());
+		
+		$this->popular_track_iterator = API::getDataModel()->where('status = ?', STATUS_ENABLED)
+			->orderBy('listen_count', 'DESC')
+			->limit(10)
+			->loadAll(new Track());
+		
 		if ( true === $this->is_logged_in ) {
 			$this->upload_form = $this->render('track/upload-form');
 		} else {
--- httpdocs/lib/Object/Track.php
+++ httpdocs/lib/Object/Track.php
@@ -3,5 +3,30 @@
 require_once 'DataModeler/DataObject.php';
 
 class Track extends DataObject {
-
+	public function getFormattedLength() {
+		$length = $this->getLength();
+		
+		$hours = floor($length / 3600);
+		if ( $hours > 0 ) {
+			$length -= $hours * 3600;
+		}
+		
+		$minutes = floor($length / 60);
+		if ( $minutes > 0 ) {
+			$length -= $minutes * 60;
+		}
+		
+		$seconds = floor($length % 60);
+		
+		$format = $seconds . 's';
+		if ( $minutes > 0 ) {
+			$format = $minutes . 'm ' . $format;
+		}
+		
+		if ( $hours > 0 ) {
+			$format = $hours . 'h ' . $format;
+		}
+		
+		return $format;
+	}
 }
