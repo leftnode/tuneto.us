4a69f20e44f6d1feec3cdcb05fd960b06061df63
Vic Cherubini <vmc@leftnode.com>
2010-02-01 13:47:44 -0600

    Got the front page getting the latest tracks.

--- httpdocs/application/Index/Index.php
+++ httpdocs/application/Index/Index.php
@@ -5,6 +5,14 @@ require_once 'application/Root/Root.php';
 class Index_Controller extends Root_Controller {
 	
 	public function indexGet() {
+		/* Get the last 5 tracks. */
+		$track_iterator = TuneToUs::getDataModel()->where('status = ?', STATUS_ENABLED)
+			->orderBy('date_create', 'DESC')
+			->limit(5)
+			->loadAll(new Track());
+			
+		$this->track_iterator = $track_iterator;	
+		
 		$this->renderLayout('index');
 	}
 	
