7bbf830e41cf1c2f4f66885efee9752ad123e9f5
Vic Cherubini <vmc@leftnode.com>
2010-02-02 20:35:38 -0600

    Added doxygen comments.

--- httpdocs/application/Image/Image.php
+++ httpdocs/application/Image/Image.php
@@ -1,20 +1,49 @@
 <?php
 
+/**
+ * Displays an image directly to the browser.
+ * 
+ * @author vmc <vmc@leftnode.com>
+ */
 class Image_Controller extends Artisan_Controller {
+	
+	/// Do not display any layout with the image.
 	protected $layout = NULL;
 	
+	/**
+	 * Display large images to the browser.
+	 * 
+	 * @param integer $image_id The ID of the image to display.
+	 */
 	public function viewLargeGet($image_id) {
 		$this->viewGet($image_id, IMAGE_LARGE);
 	}
 	
+	/**
+	 * Display thumbnail images to the browser.
+	 * 
+	 * @param integer $image_id The ID of the image to display.
+	 */
 	public function viewThumbnailGet($image_id) {
 		$this->viewGet($image_id, IMAGE_THUMBNAIL);
 	}
 	
+	/**
+	 * Display micro images to the browser.
+	 * 
+	 * @param integer $image_id The ID of the image to display.
+	 */
 	public function viewMicroGet($image_id) {
 		$this->viewGet($image_id, IMAGE_MICRO);
 	}
 	
+	/**
+	 * Display images to the browser. If the image does not exist,
+	 * a generic image is displayed in the same size.
+	 * 
+	 * @param integer $image_id The ID of the image to display.
+	 * @param string $size The size of the image to display.
+	 */
 	private function viewGet($image_id, $size) {
 		try {
 			$image_id = intval($image_id);
