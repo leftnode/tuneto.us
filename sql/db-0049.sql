d22778039109be1dd784459976e00f7b15f2e53c
Vic Cherubini <vmc@leftnode.com>
2010-02-03 06:39:31 -0600

    Added the dump for the actual emails.

--- sql/db-0049.sql
+++ sql/db-0049.sql
@@ -9,3 +9,6 @@ CREATE TABLE `email` (
   `alt_body` text collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`email_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `email` (`email_id`, `date_create`, `date_modify`, `locale`, `name`, `subject`, `body`, `alt_body`) VALUES
+(1, 1265199899, 0, 'en_US.utf8', 'track-processed', 'Your Track Is Processed!', 'Hello {nickname},\r\n\r\nYour track has been processed and is ready to view! Click below to listen to it or share it with your friends.\r\n\r\n{track_url}\r\n\r\n-{from_name}', 'Hello {nickname},\r\n\r\nYour track has been processed and is ready to view! Click below to listen to it or share it with your friends.\r\n\r\n{track_url}\r\n\r\n-{from_name}');
