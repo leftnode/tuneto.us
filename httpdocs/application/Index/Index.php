0c8818f19393f6915ecf8e5be77bfe9b21e433fe
Vic Cherubini <vmc@leftnode.com>
2010-02-03 20:07:51 -0600

    Moved the browsing code to the index controller rather than having it's own controller.

--- httpdocs/application/Index/Index.php
+++ httpdocs/application/Index/Index.php
@@ -6,12 +6,42 @@ class Index_Controller extends Root_Controller {
 	
 	public function indexGet() {
 		/* Get the last 5 tracks. */
-		$this->track_iterator = TuneToUs::getDataModel()->where('status = ?', STATUS_ENABLED)
+		$this->track_iterator = TuneToUs::getDataModel()
+			->where('status = ?', STATUS_ENABLED)
 			->orderBy('date_create', 'DESC')
 			->limit(5)
 			->loadAll(new Track());
 		
 		$this->renderLayout('index');
+	}
+	
+	public function browseGet() {
+		/* Load 20 random tracks. */
+		$db = TuneToUs::getDb();
+		
+		$sql = "SELECT * FROM `track`
+			WHERE (`status` = ?)
+			ORDER BY RAND() DESC
+			LIMIT 20";
+		$statement_track_list = $db->prepare($sql);
+		$statement_track_list->execute(array(STATUS_ENABLED));
+		
+		$track = new Track();
+		$track_list_data = array();
+
+		if ( $statement_track_list->rowCount() > 0 ) {
+			$track_data = $statement_track_list->fetchAll(PDO::FETCH_ASSOC);
+			
+			foreach ( $track_data as $track_item ) {
+				$track_list_data[] = clone $track->model($track_item);
+			}
+		}
+		
+		$track_list = new DataIterator($track_list_data);
+		
+		$this->track_list = $track_list;
+		
+		$this->renderLayout('browse');
 	}
 	
 	public function listGet() {
